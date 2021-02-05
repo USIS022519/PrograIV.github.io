e0e2f435b27a9abce37b9474467a729717b22d32
Prueba <usis022519@ugb.edu.sv>
2021-02-05 10:14:38 -0600

    Crud terminado

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 var miDBAlumnos = openDatabase('dbAlumnos', '1.0', 'Aplicacion de registro de alumnos',5*1024*1024);
 
-window.id = 0;
+var generarIdUnicoDesdeFecha=()=>{
+    let fecha = new Date(); // 05/02/21
+    return Math.floor(fecha.getTime()/1000).toString(16);
+};
 
 if(!miDBAlumnos){
     alert("El navegador no soporta web sql");
@@ -9,6 +12,10 @@ if(!miDBAlumnos){
 var appVue = new Vue({
     el: '#appAlumnos',
     data:{
+        accion : 'nuevo',
+        msg : '',
+        status : false,
+        error : false,
         alumno:{
             idAlumno : 0,
             codigo : '',
@@ -25,14 +32,32 @@ var appVue = new Vue({
     methods:{
         guardarAlumno(){
             /*db web sql*/
+
+            let sql = '';
+            if(this.accion == 'nuevo'){
+                this.alumno.idAlumno = generarIdUnicoDesdeFecha();
+                sql = ' INSERT INTO alumnos(codigo,nombre,direccion,municipio,departamento,telefono,fechaN,sexo,idAlumno) VALUES (?,?,?,?,?,?,?,?,?)';
+            }else if (this.accion == 'modificar'){
+                sql = 'UPDATE alumnos SET codigo=?,nombre=?,direccion=?,municipio=?,departamento=?,telefono=?,fechaN=?,sexo=? where idAlumno=?';
+            }
             miDBAlumnos.transaction(tran=> {
-                tran.executeSql('INSERT INTO alumnos(idAlumno,codigo,nombre,direccion,municipio,departamento,telefono,fechaN,sexo) VALUES (?,?,?,?,?,?,?,?,?)',
-                [++id,this.alumno.codigo,this.alumno.nombre,this.alumno.direccion,this.alumno.municipio,this.alumno.departamento,this.alumno.telefono,
-                    this.alumno.fechaN,this.alumno.sexo]);
+                tran.executeSql( sql,
+                [this.alumno.codigo,this.alumno.nombre,this.alumno.direccion,this.alumno.municipio,this.alumno.departamento,this.alumno.telefono,
+                    this.alumno.fechaN,this.alumno.sexo,this.alumno.idAlumno]);
                     this.obtenerAlumnos();
                     this.limpiar();
+                    this.status = true;
+                    this.msg = 'Registro exitoso.';
+                    this.error = false;
+
+                    setTimeout(()=>{
+                        this.status = false;
+                        this.msg = '';
+                    }, 3000)
                }, err=> {
-                   console.log( err );
+                   this.status = true;
+                   this.msg = err.message;
+                   this.error = true;
                });
            },
            
@@ -49,9 +74,12 @@ var appVue = new Vue({
 
         mostrarAlumno(alum){
         this.alumno = alum;
+        this.accion= 'modificar';
         },
     
         limpiar(){
+            this.accion = 'nuevo';
+            this.alumno.idAlumno='';
             this.alumno.codigo='';
             this.alumno.nombre='';
             this.alumno.direccion='';
@@ -60,6 +88,18 @@ var appVue = new Vue({
             this.alumno.telefono='';
             this.alumno.fechaN='';
             this.alumno.sexo='';
+        },
+
+        eliminarAlumno(alum){
+            if (confirm('Seguro que deseas eliminar el registro: ${alum.nombre}') ){
+                miDBAlumnos.transaction(tran=>{
+                    tran.executeSql('DELETE FROM alumnos WHERE idAlumno=?',
+                        [alum.idAlumno]);
+                    this.obtenerAlumnos();
+                }, err=>{
+                    console.log( err );
+                });
+            }
         }
 
     },
