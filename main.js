55081eabe0167bca9116c5632f0a6ea1758a1d3e
Prueba <usis022519@ugb.edu.sv>
2021-02-08 10:38:29 -0600

    Con LocalStorage

--- main.js
+++ main.js
@@ -31,45 +31,31 @@ var appVue = new Vue({
     },
     methods:{
         guardarAlumno(){
-            /*db web sql*/
+            /*Db LocalStorage*/
 
-            let sql = '';
-            if(this.accion == 'nuevo'){
-                this.alumno.idAlumno = generarIdUnicoDesdeFecha();
-                sql = ' INSERT INTO alumnos(codigo,nombre,direccion,municipio,departamento,telefono,fechaN,sexo,idAlumno) VALUES (?,?,?,?,?,?,?,?,?)';
-            }else if (this.accion == 'modificar'){
-                sql = 'UPDATE alumnos SET codigo=?,nombre=?,direccion=?,municipio=?,departamento=?,telefono=?,fechaN=?,sexo=? where idAlumno=?';
-            }
-            miDBAlumnos.transaction(tran=> {
-                tran.executeSql( sql,
-                [this.alumno.codigo,this.alumno.nombre,this.alumno.direccion,this.alumno.municipio,this.alumno.departamento,this.alumno.telefono,
-                    this.alumno.fechaN,this.alumno.sexo,this.alumno.idAlumno]);
-                    this.obtenerAlumnos();
-                    this.limpiar();
-                    this.status = true;
-                    this.msg = 'Registro exitoso.';
-                    this.error = false;
+           if (this.accion == 'nuevo'){
+            this.alumno.idAlumno = generarIdUnicoDesdeFecha();
+           }
 
-                    setTimeout(()=>{
-                        this.status = false;
-                        this.msg = '';
-                    }, 3000)
-               }, err=> {
-                   this.status = true;
-                   this.msg = err.message;
-                   this.error = true;
-               });
-           },
+           localStorage.setItem(this.alumno.idAlumno, JSON.stringify(this.alumno));
+           this.obtenerAlumnos();
+           this.limpiar();
+           this.status = true;
+           this.msg = 'Registro exitoso.';
+           this.error = false;
+
+           setTimeout(()=>{
+               this.status = false;
+               this.msg = '';
+           }, 3000);
+        },
            
            obtenerAlumnos(){
-            miDBAlumnos.transaction(tran=>{
-                tran.executeSql('SELECT * FROM alumnos',[],(index,data)=>{
-                    this.alumnos = data.rows;
-                    id=data.rows.length;
-                });
-            }, err=>{
-                console.log( err );
-            });
+            this.alumnos = [];
+            for (let index = 0; index < localStorage.length; index++) {
+                let key = localStorage.key(index);
+                this.alumnos.push( JSON.parse(localStorage.getItem(key)) );
+            }
         },
 
         mostrarAlumno(alum){
@@ -91,25 +77,15 @@ var appVue = new Vue({
         },
 
         eliminarAlumno(alum){
-            if (confirm('Seguro que deseas eliminar el registro: ${alum.nombre}') ){
-                miDBAlumnos.transaction(tran=>{
-                    tran.executeSql('DELETE FROM alumnos WHERE idAlumno=?',
-                        [alum.idAlumno]);
-                    this.obtenerAlumnos();
-                }, err=>{
-                    console.log( err );
-                });
+            if (confirm(`Seguro que deseas eliminar el registro: ${alum.nombre}`) ){
+               localStorage.removeItem(alum.idAlumno)
+               this.obtenerAlumnos();
             }
         }
 
     },
 
     created(){
-        miDBAlumnos.transaction(tran=>{
-        tran.executeSql('CREATE TABLE IF NOT EXISTS alumnos(idAlumno int PRIMARY KEY NOT NULL, codigo varchar(10), nombre varchar(40), direccion varchar(50), municipio varchar(50), departamento varchar(25), telefono varchar(10), fechaN date, sexo varchar(10))');
-        }, err=>{
-            console.log( err );
-        });
         this.obtenerAlumnos();
     }
 });
