b0df71ce7d90dd1ee1b975477417f28cb02c49a6
Josué Torres <usis022519@ugb.edu.sv>
2021-03-19 20:44:44 -0600

    Inicio del parcial y creacion de base de datos con indexed DB

--- main.js
+++ main.js
@@ -1,69 +1,52 @@
-var generarIdUnicoDesdeFecha=()=>{
-    let fecha = new Date();//03/02/2021
-    return Math.floor(fecha.getTime()/1000).toString(16);
+var generarIdUnicoDesdeFecha = () => {
+    let fecha = new Date(); //03/02/2021
+    return Math.floor(fecha.getTime() / 1000).toString(16);
 }, db;
 var appVue = new Vue({
-    el:'#appSistema',
-    data:{
-        forms:{
-            'registro_alumnos':{mostrar:false},
-            'matricula':{mostrar:false},
-            'materias':{mostrar:false},
-            'inscripcion':{mostrar:false},
+    el: '#appSistema',
+    data: {
+        forms: {
+            'cliente': { mostrar: false },
+            'lectura': { mostrar: false },
         }
     },
-    methods:{
-        abrirBd(){
-            let indexDb = indexedDB.open('db_sistema_academico',1);
-            indexDb.onupgradeneeded=event=>{
-                let req=event.target.result,
+    methods: {
+        abrirBd() {
+            let indexDb = indexedDB.open('db_sistema_agua_potable', 1);
+            indexDb.onupgradeneeded = event => {
+                let req = event.target.result,
 
-                    tblmatricula = req.createObjectStore('tblmatricula',{keyPath:'idMatricula'}),
-                    tblregistro = req.createObjectStore('tblregistro',{keyPath:'idRegistro'}),
-                    tblmateria = req.createObjectStore('tblmateria',{keyPath:'idMateria'}),
-                    tblinscripcion = req.createObjectStore('tblinscripcion',{keyPath:'idInscripcion'})
+                    tblclientes = req.createObjectStore('tblclientes', { keyPath: 'idCliente' }),
+                    tbllecturas = req.createObjectStore('tbllecturas', { keyPath: 'idLectura' })
 
-                tblmatricula.createIndex('idMatricula','idMatricula',{unique:true});
-                tblmatricula.createIndex('codigo','codigo',{unique:false});
-                tblmatricula.createIndex('id','id',{unique:false});
-                
-                tblregistro.createIndex('idRegistro','idRegistro',{unique:true});
-                tblregistro.createIndex('codigo','codigo',{unique:false});
+                tbllecturas.createIndex('idLectura', 'idLectura', { unique: true });
 
-                tblmateria.createIndex('idMateria','idMateria',{unique:true});
-                tblmateria.createIndex('codigo','codigo',{unique:false});
-
-                tblinscripcion.createIndex('idInscripcion','idInscripcion',{unique:true});
-                tblinscripcion.createIndex('codigo','codigo',{unique:false});
-
-     
+                tblclientes.createIndex('idCliente', 'idCliente', { unique: true });
+                tblclientes.createIndex('codigo', 'codigo', { unique: false });
             };
-            indexDb.onsuccess = evt=>{
-                db=evt.target.result;
+
+            indexDb.onsuccess = evt => {
+                db = evt.target.result;
             };
-            indexDb.onerror=e=>{
+            indexDb.onerror = e => {
                 console.log("Error al conectar a la BD", e);
             };
         }
     },
-    created(){
+    created() {
         /**
-             * Estas son las bases de datos con las que se ha trabajado.
-             * webSQL -> DB Relacional en el navegador
-             * localStorage -> BD NOSQL clave/valor
-             * indexedDB -> BD NOSQL clave/valor
-             */
+          * indexedDB -> BD NOSQL clave/valor
+         */
         this.abrirBd();
     }
 });
 
-document.addEventListener("DOMContentLoaded",event=>{
-    let el = document.querySelectorAll(".mostrar").forEach( (element, index)=>{
-        element.addEventListener("click",evt=>{
+document.addEventListener("DOMContentLoaded", event => {
+    let el = document.querySelectorAll(".mostrar").forEach((element, index) => {
+        element.addEventListener("click", evt => {
             appVue.forms[evt.target.dataset.form].mostrar = true;
             appVue.$refs[evt.target.dataset.form].obtenerDatos();
         });
-    } );
+    });
 });
-     
-        
+
